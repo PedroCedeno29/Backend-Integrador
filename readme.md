db48d470926fd010b2824e654d2a4c9cc0171e80
Pedro Cedeño <pcedenoj29@gmai.com>
2025-01-09 15:47:18 -0500

    feat(user.json): :sparkles: Se hizo una simulaciòn de una base de datos y se modifico el archivo repository.ts

--- readme.md
+++ readme.md
@@ -3,5 +3,5 @@ Node 20.18.1 / Express
 
 ### Deploy / Levantamiento
 1. Crear un archivo .env basandose en el archivo .env.example y rellenar las variables de entorno
-
-2. Levantar el proyecto con el comando ``npm run dev``
+2. Instalación de dependencias con `npm install`
+3. Levantar el proyecto con el comando ``npm run dev``
--- src/modules/auth/repository.ts
+++ src/modules/auth/repository.ts
@@ -1,20 +1,50 @@
 import { UserI } from "../../interfaces/Auth.interface";
 
+//fs: file system
+//Esta libreria es la que nos va permitir leer y escribir archivos dentro de nuestro proyecto de node.
+import fs from 'fs/promises'
+
+//Esto sirve para leer la ruta del sistema operativos de manera correcta en nuestro proyecto.
+import path from 'path';
+
+
 //Arreglo para simular un registro de usuarios en el repositorio.
 //Arreglo de tipo: interface UserI pero como arreglo y esta variable es igual a un arreglo.
 const users: UserI[] = []
 
+//Se crea una variable para almacenar la ruta de nuestro archivo en el S.O.
+const dataFilePath = path.join('src','data','users.json');
+
 //Aqui se le pone el nombre de Auth al inicio ya que viene de la subcarpeta auth, esto hace referencia al modulo auth y al archivo en que se esta trabajando, en este caso Repositoy.
 export default class AuthRepository{
+
+    async readUsers(): Promise<UserI[]> {
+        try {
+            //metodo fd.readFile: sirve para leer el contenido del archivo que le pasamos por parametro.
+            const data = await fs.readFile(dataFilePath, 'utf-8') //utf8 es para asignarle un tipo de texto al archivo
+            return JSON.parse(data);
+        } catch (error) {
+            return []
+        }
+    }
+
+    async writeUsers(users: UserI[]): Promise<void>{
+        //EL metodo writeFile nos sirve para escribir en el archivo user.json
+        //Hay que enviar al archivo.json la data serializada, es transformar un objeto a string
+        await fs.writeFile(dataFilePath, JSON.stringify(users, null, 2), 'utf-8');
+    }
     //Metodo de la clase: Lo que va a hacer nuestro repositorio. Simular la conexion con la base de datos.
     //El Promise requiere del argumento de tipo genérico (en este caso va a retornar el UserI).
     //El parametro 'user' es de tipo UserI es decir que es del tipo de la interfaz que se creó en el archivo Auth.interfaces.ts que contiene 2 propiedas username y password.
     async createUser(user: UserI): Promise<UserI>{      //Dentro de las <> va el tipo de dato que va a retornar la funcion async
-       users.push(user);
+       const users = await this.readUsers();
+        users.push(user);
+        await this.writeUsers(users);
         return user;                                    //Se recomienda usar un return como una confirmacion
     }
 
     async findByUsername(username: string): Promise<UserI | undefined>{
+        const users = await this.readUsers();
         //Esta funcion recibe un callback como parametro de la funcion find()
         return users.find((user) => user.username === username); //Cuando es una comparación de una sola línea no es necesaria usar las llaves y el return, esto sería de manera implícita.
 
